da5c4f46bdd2a1b5b6f86e39885d8a16e1ced356
ramouzacky <ramouz.muzacky@gmail.com>
2018-10-12 15:29:43 +0700

    Table Component

--- src/views/CommonComponent.js
+++ src/views/CommonComponent.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { LocaleProvider, Layout, Form, Input, Menu, Select,Checkbox,
-  Modal, Icon, Avatar, Table, Button, Row, Col, Card
+import { LocaleProvider, Layout, Form, Input, Menu, Select,Checkbox, Radio, DatePicker,
+  Modal, Icon, Avatar, Table, Button, Row, Col, Card, Divider, Tag
 } from 'antd';
 const {TextArea} = Input;
 const CheckboxGroup = Checkbox.Group;
+const RadioGroup = Radio.Group;
 const Option = Select.Option;
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 class CommonComponent extends React.Component{
+  state = {
+    value: 1,
+  }
   renderDetailsOption = ()=> {
     var DetailsOption = [];
     DetailsOption     = this.props.options.map((obj,i)=>{
@@ -18,14 +23,57 @@ class CommonComponent extends React.Component{
   handleChangeDropDown = () => {
 
   }
+  onChange = (date, dateString) => {
+  }
+  
   render (){
     var title = this.props.title ? this.props.title :'';
     var type  = this.props.type ? this.props.type :'';
     var span  = this.props.span ? this.props.span : 24;
     var value = this.props.value ? this.props.value :'';
-    var options= this.props.options ? this.props.options : [];
-    var placeholder = this.props.placeholder ? this.props.placeholder :'';
-    var disabled = this.props.disabled ? this.props.disabled :true;
+    var color = this.props.value ? this.props.color :'primary';
+    var options      = this.props.options ? this.props.options : [];
+    var placeholder  = this.props.placeholder ? this.props.placeholder :'';
+    var disabled     = this.props.disabled ? this.props.disabled :true;
+    const radioStyle = {
+      display: 'block',
+      height: '30px',
+      lineHeight: '30px',
+    };
+
+    const columns = [{
+      title: 'Column1',
+      dataIndex: 'name',
+      key: 'name',
+      // render: text => <a href="javascript:;">{text}</a>,
+    }, {
+      title: 'Column2',
+      dataIndex: 'age',
+      key: 'age',
+    }, {
+      title: 'Column3',
+      dataIndex: 'address',
+      key: 'address',
+    },];
+    
+    const data = [{
+      key: '1',
+      name: 'Value 1',
+      age: 15,
+      address: 'Podium Depan',
+    }, {
+      key: '2',
+      name: 'Value 2',
+      age: 15,
+      address: 'Podium Tengah',
+    }, {
+      key: '3',
+      name: 'Value 3',
+      age: 15,
+      address: 'Podium Belakang',
+
+    }];
+
     var handleChangeInputNumber = this.props.handleChangeInputNumber ? this.props.handleChangeInputNumber : () => {};
     switch (type) {
       case 'textinput':
@@ -77,6 +125,11 @@ class CommonComponent extends React.Component{
       case 'dropdown' :
        return (
          <Col span={span} style={{marginBottom: 15, paddingBottom: 10}}>
+            {this.props.title ?
+              <span style={{marginBottom: 10}}>{title}</span>
+              :
+              []
+            }
            <Select
              disabled={disabled}
              showSearch
@@ -86,17 +139,66 @@ class CommonComponent extends React.Component{
              onChange={()=>this.handleChangeDropDown()}
              filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
            >
-             <Option value="jack">Jack</Option>
-             <Option value="lucy">Lucy</Option>
-             <Option value="tom">Tom</Option>
+             <Option value="jack">Option 1</Option>
+             <Option value="lucy">Option 2</Option>
+             <Option value="tom">Option 3</Option>
            </Select>
         </Col>
        )
       break;
+      case 'button' :
+        var backgrounColor = '#22ff22 !important';
+        return (
+          <Col span={span} style={{marginBottom: 15, paddingBottom: 10}}>
+          <div>
+            <Button color={'#22ff22'} style={{paddingLeft:10, paddingRight:10, marginTop: 20, backgrounColor:backgrounColor}}  type={color}>{value}</Button>
+          </div>
+        </Col>
+        )
+      break;
+      case 'radio' :
+        return (
+          <Col span={span} style={{marginBottom:15,paddingBottom:10}}>
+            {this.props.title ?
+              <span style={{marginBottom: 10}}>{title}</span>
+              :
+              []
+            }
+          <RadioGroup style={{width:365}} onChange={this.onChange} value={this.state.value}>
+            <Radio style={radioStyle} value={1}>Option 1</Radio>
+            <Radio style={radioStyle} value={2}>Option 2</Radio>
+            <Radio style={radioStyle} value={3}>Option 3</Radio>
+          </RadioGroup>
+          </Col>
+        );
+      break;
+      case 'date' :
+        return (
+          <Col span={span} style={{marginBottom: 15, paddingBottom: 10,marginTop:20}}>
+            {this.props.title ?
+              <span style={{marginBottom: 10}}>{title}</span>
+              :
+              []
+            }
+              <DatePicker  style={{width:300}} onChange={()=>this.onChange()} />
+          </Col>
+        );
+      break;
+      case 'table':
+        return(
+          <Col span={span} style={{marginBottom:15,paddingBottom:10,marginTop:20}}>
+            {this.props.title ?
+              <span style={{marginBottom: 10}}>{title}</span>
+              :
+              []
+            }
+            <Table columns={columns} dataSource={data} /> 
+          </Col>
+        )
       default:
         return(
           <div>
-          <span style={{color:'red', fontSize: 14, fontWeight: '500'}}>Component ini belum dibuat !</span>
+          <span  style={{color:'red', fontSize: 14, fontWeight: '500'}}>Component ini belum dibuat !</span>
           </div>
         )
     }
